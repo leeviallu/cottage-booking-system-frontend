528ef197be24ecc7bc20a7c4341d92f0351e04f6
leeviallu <68004427+leeviallu@users.noreply.github.com>
2024-05-11 01:22:14 +0300

    Cleaned billings

--- src/Billing.jsx
+++ src/Billing.jsx
@@ -1,96 +1,48 @@
+import { useEffect, useState } from "react";
 
-import axios from "axios";
-import { useState, useEffect } from 'react';
+const Billing = ({bor, billingsOfReservation}) => {
+    const [billingServices, setBillingServices] = useState([]);
+    const sor = bor[0];
+    const billing = bor[1];
 
-const Billing = ({billing}) => {
-    const [editing, setEditing] = useState(false);
-    const [formData, setFormData] = useState({
-        reservationId: billing.reservationId,
-        sum: billing.sum,
-        isPaid: billing.isPaid
-    });
-    const handleChange = (e) => {
-        setFormData({ ...formData, [e.target.name]: e.target.value });
-    };
+    useEffect(() => {
+        const updatedBillingServices = billingsOfReservation
+            .filter(bor => bor[1].billingId === billing.billingId)
+            .map(bor => bor[0].service);
+    
+        setBillingServices(updatedBillingServices);
+    }, [billingsOfReservation, billing.billingId]);
+    
 
+    return (
+        <div key={billing.billingId}>
+            <p>{sor.reservation.customer.firstname} {sor.reservation.customer.lastname}</p>
+            <p>{sor.reservation.customer.email}</p>
+            <p>{sor.reservation.customer.phonenumber}</p>
+            <p>{sor.reservation.cottage.name}</p>
+            <p>EI TOIMI {billing.sum}</p>
+            
+            {
+                billingServices.map(service => (
+                    <div key={service.serviceId}>
+                        <p><b>Service: </b>{service.name}</p>
+                        <p><b>Price: </b>{service.price}</p>
+                    </div>
+                ))
+            }
+            <p>Reservation between {new Date(sor.reservation.reservationStartingDate).toISOString().split('T')[0]} and {new Date(sor.reservation.reservationEndingDate).toISOString().split('T')[0]}</p>
+            
+            {
+                billing.isPaid
+                ?
+                <h3>maksettu</h3>
+                :
+                <h3>ei maksettu</h3>
 
-const handleDelete = async (event, id) => {
-    event.preventDefault();
-    try {
-        axios.delete(`http://localhost:8080/api/billings/${id}`);
-        
-    } catch (error) {
-        console.error('Error deleting billing:', error);
-    }
-    window.location.reload();
-};
+            }
 
-const handleEdit = async (event, id) => {
-    const {reservationId, sum, isPaid} = formData;
-    const billingId=billing.billingId
-    event.preventDefault();
-    console.log(formData)
-    axios.put(`http://localhost:8080/api/billings/${billingId}`, 
-    {
-
-        reservationId:reservationId,
-                sum:sum,
-                isPaid:isPaid,
-
-    }
+            <br/>
+        </div>
     )
-    window.location.reload();
-};
-return(<div>
-    {
-        editing
-    ?
-    <form onSubmit={handleEdit}>
-    
-         <br />
-        <label>
-        ReservationId:
-        <input type="number" name="reservationId" value={formData.reservationId} onChange={handleChange} />
-        </label>
-        <br />
-        <label>
-        Sum:
-        <input type="number" name="sum" value={formData.sum} onChange={handleChange} />
-        </label>
-        <br />
-        <label>
-        isPaid:
-        <input type="text " name="isPaid" value={formData.isPaid} onChange={handleChange} />
-        </label>
-        <br/>              
-        <button type="submit">Submit</button>
-        <br/>
-        <br/>
-        <button onClick={() => setEditing(!editing)}>Edit</button>
-
-        <button onClick={(event) => handleDelete(event, billing.billingId)}>Remove</button>
-        <br/>
-    </form>
-
-    :
-    <div key={billing.billingId}>
-        <p>
-        <b>ReservationId:</b> {billing.reservationId}
-        <br/>
-        <b>Sum:</b> {billing.sum}
-        <br/>            
-        <b>isPaid:</b> {billing.isPaid}
-        <br/>
-        </p>
-        <button onClick={() => setEditing(!editing)}>Edit</button>
-        <button onClick={(event) => handleDelete(event, billing.billingId)}>Remove</button>
-        <br/>
-        <br/>
-        <br/>
-    </div>
-    }
-</div>
-    
-);
 };
 export default Billing;
--- src/Billings.jsx
+++ src/Billings.jsx
@@ -2,91 +2,32 @@ import axios from "axios";
 import { useState,useEffect } from 'react';
 import Billing from "./Billing";
 const Billings=()=>{
-    const [billings, setBillings] = useState([]);
-    const[formData, setFormData]=useState({
-        reservationId: '',
-        sum:'',
-        isPaid:''
-    })
-    const handleChange = (e) => {
-        setFormData({ ...formData, [e.target.name]: e.target.value });
-    };
-    const handleCreate= async (e)=>{
-        e.preventDefault();
-        const {reservationId, sum, isPaid} = formData;
-        try {
-            await axios.post('http://localhost:8080/api/billings', {
-                reservationId:reservationId,
-                sum:sum,
-                isPaid:isPaid,
-            });
-            
-            setFormData({
-                reservationId: '',
-                sum: '',
-                isPaid: '',
-            });
-            fetchBillings();
-        } catch (error) {
-            console.error('Error creating billing:', error);
-        }
-        window.location.reload();
-    }
+    const [billingsOfReservation, setBillingsOfReservation] = useState([]);
 
-    const fetchBillings = async () => {
-        try {
-            const fetchedBillings = await axios.get('http://localhost:8080/api/billings');
-            setBillings(fetchedBillings.data);
-            console.log(fetchBillings.data);
-        } catch (error) {
-            console.error('Error fetching Billings:', error);
-        }
-    };
     useEffect(() => {
-        const fetchBillings = async () => {
-            try {
-                const fetchedBillings = await axios.get('http://localhost:8080/api/billings');
-                setBillings(fetchedBillings.data);
-                console.log(fetchBillings.data);
-            } catch (error) {
-                console.error('Error fetching Billings:', error);
-            }
-        }
-        fetchBillings();
+        axios.get(`http://localhost:8080/api/billings/reservation?confirmationDate=${new Date().toISOString().split('T')[0]}`)
+            .then(res => {
+                setBillingsOfReservation(res.data);
+            })
+            .catch(e => {
+                console.error('Error fetching billingsOfReservation: ',e);
+            })
     }, []);
 
     
     return (
         <div>
-            <h1>Create Billing</h1>
-            <form onSubmit={handleCreate}>
-                <br />
-                <label>
-                    ReservationId:
-                    <input type="number" name="reservationId" value={formData.reservationId} onChange={handleChange} />
-                </label>
-                <br />
-                <label>
-                    Sum:
-                    <input type="number" name="sum" value={formData.sum} onChange={handleChange} />
-                </label>
-                <br />
-                <label>
-                    is Paid:
-                    <input type="text " name="isPaid" value={formData.isPaid} onChange={handleChange} />
-                </label>
-                <br />
-                <button type="create">Create</button>
-            </form>
             <h2>All billings</h2>
-            <ul>
-            {
-                    billings.map(billing=>
-                        <Billing key={billing.billingId} billing={billing}/>
-                    )
+            <div>
+                {
+                    billingsOfReservation
+                        .filter((bor, index, self) => self.findIndex(b => b[1].billingId === bor[1].billingId) === index)
+                        .map(bor =>
+                            <Billing key={bor[1].billingId} bor={bor} billingsOfReservation={billingsOfReservation} />
+                        )
                 }
-            </ul>
-    </div>
+            </div>
+        </div>
     );
 };
 export default Billings;
