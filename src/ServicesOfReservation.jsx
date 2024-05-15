51dbff2ae3f35a368f049a8afa77fe35814d5dd9
lauri22 <lauri.kuivisto56@gmail.com>
2024-05-15 16:44:10 +0300

    korjailuu

--- src/ServicesOfReservation.jsx
+++ src/ServicesOfReservation.jsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react";
 import axios from "axios";
+import Reservation from "./Reservation";
 
 const ServicesOfReservation = () => {
     const [reservations, setReservations] = useState([])
@@ -9,10 +10,15 @@ const ServicesOfReservation = () => {
         serviceId: "",
         count: 0,
     });
+    const[sorSearchResult, setSorSearchResult] = useState([])
+    const[reservationId, setReservationId]=useState(0)
 
     const handleChange = (e) => {
         setFormData({ ...formData, [e.target.name]: e.target.value });
     };
+    const handleCustChange = (e) => {
+        setReservationId(e.target.value)
+    }
 
     const handleSubmit = (e) => {
         e.preventDefault();
@@ -50,8 +56,27 @@ const ServicesOfReservation = () => {
                 console.error("Error while creating Services of Reservation: ", e)
             })
     }
+ 
+    const handleDelete = (serviceId, reservationId) => {
+        axios.delete(`http://localhost:8080/api/sor/${reservationId}/${serviceId}`)
+            .then(() => {
+                setSorSearchResult(sorSearchResult.filter(sor => sor.serviceId !== serviceId));
+            }).catch(e => {
+                console.error("Error deleting service of reservation: ", e);
+            });
+    };
 
 
+    const searchSor =(e) => {
+        e.preventDefault();
+        axios.get(`http://localhost:8080/api/sor/${reservationId}`)
+            .then(res => {
+                setSorSearchResult(res.data);
+                console.log(res.data)
+            }).catch(e => {
+                console.error('Error fetching services: ', e)
+            })
+    }
     useEffect(() => {
         axios.get('http://localhost:8080/api/reservations')
             .then(res => {
@@ -75,6 +100,7 @@ const ServicesOfReservation = () => {
             }).catch(e => {
                 console.error('Error fetching services: ', e)
             })
+            
         
     // eslint-disable-next-line react-hooks/exhaustive-deps
     }, [])
@@ -95,7 +121,7 @@ const ServicesOfReservation = () => {
                     return(
                         <option key={reservation.reservationId} value={reservation.reservationId}>
                             {reservation.customer.email}
-                            { }
+                            {", "}
                             {new Date(reservation.reservationStartingDate).toISOString().split('T')[0]}
                             -
                             {new Date(reservation.reservationEndingDate).toISOString().split('T')[0]}
@@ -110,7 +136,8 @@ const ServicesOfReservation = () => {
                 {services.map((service) => {
                     return(
                         <option key={service.serviceId} value={service.serviceId}>
-                            {service.name}, 
+                            {service.name}
+                            {", "}
                             {service.description}
                         </option>
                 )})}
@@ -134,6 +161,39 @@ const ServicesOfReservation = () => {
                 <br />      
                 <button type="submit">Lisää varaukseen</button>
             </form>
+
+        <h2>Hae Varauksen palveluja</h2>
+        <div>
+        <form onSubmit={searchSor} >
+        <select id="reservationId" name="reservationId" value={reservationId} onChange={handleCustChange}>
+                {reservations.map((reservation) => {
+                    return(
+                        <option key={reservation.reservationId} value={reservation.reservationId}>
+                            {reservation.customer.email}
+                            {", "}
+                            {new Date(reservation.reservationStartingDate).toISOString().split('T')[0]}
+                            -
+                            {new Date(reservation.reservationEndingDate).toISOString().split('T')[0]}
+                        </option>
+                )})}
+                </select>
+                <button type="submit">Hae</button>
+        </form>
+        {sorSearchResult.map((sor) => {
+                    const matchedService = services.find(service => service.serviceId === sor.serviceId);
+                    return (
+                        <div key={sor.serviceId}>
+                            {matchedService && (
+                                <p>{matchedService.name}</p>
+                            )}
+                            <p>Määrä: {sor.count}</p>
+                            <button onClick={() => handleDelete(sor.serviceId, sor.reservationId)}>Poista</button>
+                        
+                        </div>
+                    );
+                })}
+        </div>
+        
         </div>
     );
 }
